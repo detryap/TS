9f3cc4c752d0203a27e6b87aecade2c757ade48e
Toddy Ezra <toddyezra98@gmail.com>
2016-11-22 22:55:31 +0700

    Siswa Guru Button

---
+++ TS/app/src/main/java/id/sch/smktelkom_mlg/learn/tsapp/Menu.java
@@ -0,0 +1,27 @@
+package id.sch.smktelkom_mlg.learn.tsapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
